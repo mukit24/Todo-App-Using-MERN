119839c6f3a7ad8d028a67ffa229c1afb5d2104f
mukit24 <mukithasanpranto4@gmail.com>
2023-05-06 22:54:34 +0600

    registration template

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { FaUser } from 'react-icons/fa';
+import { useState, UseEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Register() {
+  const [formData, setformData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e) => { 
+    setformData( (prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className="bg-light text-dark p-4">
+        <div className="container">
+          <h3 className="text-center text-primary"><FaUser /> Registration</h3>
+          <div className="row justify-content-center">
+            <div className="col-5">
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Name" name='name' value={name} onChange={handleChange} />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email"
+                    name='email' value={email} onChange={handleChange} />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password"
+                    name='password' value={password} onChange={handleChange} />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password"
+                    name='password2' value={password2} onChange={handleChange} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" className='w-100'>
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
