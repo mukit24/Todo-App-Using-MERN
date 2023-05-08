32b756874890d3c6e6ba0f2bd8f936b4b1c48a1c
mukit24 <mukithasanpranto4@gmail.com>
2023-05-08 22:28:08 +0600

    login template and basic auth slice

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { FaSignInAlt } from 'react-icons/fa';
+import { useState, UseEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
+  const [formData, setformData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setformData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className="bg-light text-dark p-4">
+        <div className="container">
+          <h3 className="text-center text-success"><FaSignInAlt /> Login</h3>
+          <div className="row justify-content-center">
+            <div className="col-5">
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email"
+                    name='email' value={email} onChange={handleChange} />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password"
+                    name='password' value={password} onChange={handleChange} />
+                </Form.Group>
+
+                <Button variant="success" type="submit" className='w-100'>
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
